ed84d06203ba104e38b2f08e19280f542abf8deb
Bruno Ardel <djbrunohardel@hotmail.com>
2017-03-19 11:38:02 -0300

    Initial commit

---
+++ src/main/java/br/com/brrf/application/Application.java
@@ -0,0 +1,21 @@
+package br.com.brrf.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by djbru on 18/03/2017.
+ */
+
+@SpringBootApplication
+
+public class Application {
+    public static void main(String[] args){
+
+        SpringApplication.run(Application.class,args);
+
+    }
+
+}

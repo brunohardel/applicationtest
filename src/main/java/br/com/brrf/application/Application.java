8016548530ee3d47f539f3aad6bcb31f965f63fa
Hardel <djbrunohardel@hotmail.com>
2017-04-14 18:41:53 -0300

    Commit da branch nova BrunoH. Alterado index.html e adicionado tags style, body.

--- src/main/java/br/com/brrf/application/Application.java
+++ src/main/java/br/com/brrf/application/Application.java
@@ -10,13 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 
 @SpringBootApplication
+@Controller
 
 public class Application {
     public static void main(String[] args){
 
         SpringApplication.run(Application.class,args);
-//Teste de Github
+
+    }
+
+    @RequestMapping (path = "/olaMundo")
+    public String olaMundo(){
+        return "Index";
     }
 
 }
-//Teste branch master Bruno

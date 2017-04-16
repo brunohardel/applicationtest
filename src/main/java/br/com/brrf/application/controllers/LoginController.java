c9d5d76e9fd7fd61c483eac58780bff8c6f4048c
Hardel <djbrunohardel@hotmail.com>
2017-04-15 21:22:12 -0300

    Criado a url login e a controler do login, alteração do metodo para post.

--- src/main/java/br/com/brrf/application/controllers/LoginController.java
+++ src/main/java/br/com/brrf/application/controllers/LoginController.java
@@ -1,18 +1,28 @@
 package br.com.brrf.application.controllers;
 
+import br.com.brrf.application.controllers.entities.Pessoa;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by rafael on 15/04/17.
  */
+@Controller
 @RequestMapping(path = "/inscricao")
 public class LoginController {
 
-    @RequestMapping("/login")
+    @RequestMapping(path = "/login")
     public String trazerLogin() {
         return "Login";
     }
 
+    @RequestMapping(path = "/logar", method = RequestMethod.POST)
+    public void logar(Pessoa pessoa) {
+
+        System.out.println("O Login é: " + pessoa.getLogin());
+        System.out.println("A Senha é: " + pessoa.getSenha());
+    }
 
 
 }
